6ce73f74b13001d65eca5467fbba5ad45931c100
SAFAE SEHLAOUI <safaesahlaoui2001@gmail.com>
2022-06-06 22:40:48 +0000

    my pfe app

---
+++ screens/MainLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+import {COLORS,SIZES,icons} from '../constants';
+
+const MainLayout=({children})=>{
+    return(
+        
+            <View
+            style={{
+                flex:1,
+
+            }}
+        >
+            {children}
+  
+        </View>
+        
+        
+    )
+}
+export default MainLayout;
---
+++ screens/index.js
@@ -0,0 +1,12 @@
+import Home from "./Home"
+import Portfolio from "./Portfolio"
+import Market from "./Market"
+import Profile from "./Profile"
+import MainLayout from "./MainLayout"
+export {
+    MainLayout,
+    Home,
+    Portfolio,
+    Market,
+    Profile
+}
